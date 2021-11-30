7fa39d4f50f56b8592e045dc6724cf3a07ce38a7
Mesut ÖZTÜRK <mesut8311006@gmail.com>
2021-11-30 14:19:06 +0300

    made some configure

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'rest_framework',
+
+    # my apps
+    'courses',
 ]
 
 MIDDLEWARE = [
